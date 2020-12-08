ccd436b8bbb3cdffff1e3c601c5ee3f5ea770806
julie-devos <julie.devos@epitech.eu>
2020-12-08 14:23:44 +0100

    initial commit

---
+++ exo6.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 6-Partie 3</title>
+</head>
+<body>
+<?php
+$i=20;
+
+while ($i>=0){ ?>
+    <p><?=$i.". C'est presque bon";?></p>
+    <?php
+    $i--;
+}
+?>
+
+</body>
+</html>
