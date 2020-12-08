9d479779a8d3d5fdda4b59c41b06a40351ae7207
julie-devos <julie.devos@epitech.eu>
2020-12-08 14:21:19 +0100

    initial commit

--- exo5.php
+++ exo5.php
@@ -2,16 +2,16 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Exercice 4-Partie 3</title>
+    <title>Exercice 5-Partie 3</title>
 </head>
 <body>
 <?php
-$var1=1;
+$i=0;
 
-while ($var1<10){ ?>
-    <?=$var1." ";?>
-    <?php $var1+=$var1/2;
-    $var1+=$var1/2;
+while ($i<=15){ ?>
+    <p><?=$i.". On y arrive presque";?></p>
+    <?php
+    $i++;
 }
 ?>
 
