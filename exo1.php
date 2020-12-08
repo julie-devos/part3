5bcba334bb50eb00ef76da8ed8f3b4b6f339aaa7
julie-devos <julie.devos@epitech.eu>
2020-12-08 12:07:05 +0100

    initial commit

---
+++ exo1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 1-Partie 3</title>
+</head>
+<body>
+<?php
+$age = 0;
+while ($age<=9){ ?>
+<p><?=$age;?></p>
+<?php $age++;
+}
+?>
+
+</body>
+</html>
