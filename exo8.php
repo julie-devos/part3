d958e2aa100f68265ac11890be990dc5893d4d47
julie-devos <julie.devos@epitech.eu>
2020-12-08 14:30:40 +0100

    initial commit

---
+++ exo8.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 7-Partie 3</title>
+</head>
+<body>
+<?php
+$i=200;
+
+while ($i>=0){ ?>
+    <p><?=$i.". Enfin !!!!";?></p>
+    <?php
+    $i-=12;
+}
+?>
+
+</body>
+</html>
